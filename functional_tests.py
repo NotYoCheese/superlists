d7aa530656ea7e25331aa535a86e453afc01f720
Michael Noe <mikejnoe@gmail.com>
2014-11-06 18:12:33 -0500

    First unit test and url mapping, dummy view

--- functional_tests.py
+++ functional_tests.py
@@ -2,23 +2,23 @@
 from selenium import webdriver
 import unittest
 
-class NewVisitorTest(unittest.TestCase):  #1
+class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self):  #2
+    def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
-        
-    def tearDown(self):  #3
+
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  #4
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)  #5
-        self.fail('Finish the test!')  #6
+        self.assertIn('To-Do', self.browser.title) 
+        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
 
@@ -42,5 +42,5 @@ class NewVisitorTest(unittest.TestCase):  #1
         # Satisfied, she goes back to sleep
 
 
-if __name__ == '__main__':  #7
-    unittest.main(warnings='ignore')  #8
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
